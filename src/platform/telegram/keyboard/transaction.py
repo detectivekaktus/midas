095a293494ddffde6fe352f40757de2326bdad4d
Artiom Astashonak <artiomastashonak@gmail.com>
2025-11-28 10:32:11 +0100

    feat: add transaction type telegram keyboard

---
+++ src/platform/telegram/keyboard/transaction.py
@@ -0,0 +1,13 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+from src.platform.telegram.util.transaction import get_transaction_type_list
+
+
+def get_transaction_type_keyboard() -> ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+    for type_ in get_transaction_type_list():
+        builder.button(text=type_)
+
+    builder.adjust(4)
+    return builder.as_markup(resize_keyboard=True)

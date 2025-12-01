8c553b6953a198b34b08f6b275a119077385744f
Artiom Astashonak <artiomastashonak@gmail.com>
2025-12-01 19:29:20 +0100

    fix: review changes #17

--- src/loggers.py
+++ src/loggers.py
@@ -1,17 +1,20 @@
 from json import loads
 from logging import getLogger
 from logging.config import dictConfig
-from os import getcwd, mkdir, path
+from pathlib import Path
 
 
 def load_logging_config() -> None:
-    with open("logging.conf.json", "r") as f:
+    conf_path = Path(__file__).parent.parent / "logging.conf.json"
+    with conf_path.open("r") as f:
         content = f.read()
 
     conf = loads(content)
-
-    if not path.exists(path.join(getcwd(), "logs")):
-        mkdir("logs")
+    if "file" in conf.get("handlers", {}):
+        filename = conf["handlers"]["file"].get("filename")
+        if filename:
+            log_dir = conf_path.parent / Path(filename).parent
+            log_dir.mkdir(parents=True, exist_ok=True)
 
     dictConfig(config=conf)
 

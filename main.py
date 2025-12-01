7fb1eb8f90f147b1f0706dc2e6fc7c2540cd2add
Artiom Astashonak <artiomastashonak@gmail.com>
2025-12-01 18:27:31 +0100

    feat: add logging json config

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from asyncio import run
-from logging import INFO, basicConfig
-from sys import stdout
 
 from src.platform.telegram import start_bot
 
 
 async def main() -> None:
-    basicConfig(level=INFO, stream=stdout)
     await start_bot()
 
 

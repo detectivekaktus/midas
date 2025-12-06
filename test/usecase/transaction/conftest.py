ad40245ebae70b9fcfa1e7ac44ef5947760be66d
Artiom Astashonak <artiomastashonak@gmail.com>
2025-12-06 10:49:32 +0100

    test: add tests for `DeleteTransactionUsecase`

--- test/usecase/transaction/conftest.py
+++ test/usecase/transaction/conftest.py
@@ -1,11 +1,18 @@
 from pytest import fixture
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.usecase.transaction import CreateTransactionUsecase
+from src.usecase.transaction import CreateTransactionUsecase, GetTransactionsUsecase
 
 
 @fixture
 def test_create_transaction(test_engine) -> CreateTransactionUsecase:
     session = AsyncSession(test_engine)
     usecase = CreateTransactionUsecase(session=session)
+    return usecase
+
+
+@fixture
+def test_get_transactions(test_engine) -> GetTransactionsUsecase:
+    session = AsyncSession(test_engine)
+    usecase = GetTransactionsUsecase(session=session)
     return usecase

df104cf867e7efaaa99cfd400fd865ed15fac8ad
Artiom Astashonak <artiomastashonak@gmail.com>
2025-12-16 19:47:46 +0100

    refactor: abstract generation of keyboards with back button

--- src/platform/telegram/keyboard/menu.py
+++ src/platform/telegram/keyboard/menu.py
@@ -1,3 +1,4 @@
+from enum import StrEnum
 from aiogram.types import ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
@@ -18,21 +19,21 @@ def get_main_menu_keyboard() -> ReplyKeyboardMarkup:
     return builder.as_markup(resize_keyboard=True)
 
 
-def get_profile_menu_keyboard() -> ReplyKeyboardMarkup:
+def _get_menu_with_back_button(
+    options: StrEnum, adjust: int = 3
+) -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
     builder.button(text=BackOption.BACK)
-    for option in ProfileMenuOption:
+    for option in options:
         builder.button(text=option)
-    builder.adjust(3)
+    builder.adjust(adjust)
 
     return builder.as_markup(resize_keyboard=True)
+
+
+def get_profile_menu_keyboard() -> ReplyKeyboardMarkup:
+    return _get_menu_with_back_button(ProfileMenuOption)  # type: ignore
 
 
 def get_transactions_menu_keyboard() -> ReplyKeyboardMarkup:
-    builder = ReplyKeyboardBuilder()
-    builder.button(text=BackOption.BACK)
-    for option in TransactionsMenuOption:
-        builder.button(text=option)
-    builder.adjust(3)
-
-    return builder.as_markup(resize_keyboard=True)
+    return _get_menu_with_back_button(TransactionsMenuOption)  # type: ignore

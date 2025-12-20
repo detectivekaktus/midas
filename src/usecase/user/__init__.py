05a665d12cdd5ad74ce6b0c37ec02632f41e946f
Artiom Astashonak <artiomastashonak@gmail.com>
2025-12-20 09:21:54 +0100

    test: add tests for `EdituserUsecase` class

--- src/usecase/user/__init__.py
+++ src/usecase/user/__init__.py
@@ -1,5 +1,11 @@
 from .register_user_usecase import RegisterUserUsecase
 from .delete_user_usecase import DeleteUserUsecase
 from .get_user_usecase import GetUserUsecase
+from .edit_user_usecase import EditUserUsecase
 
-__all__ = ("RegisterUserUsecase", "DeleteUserUsecase", "GetUserUsecase")
+__all__ = (
+    "RegisterUserUsecase",
+    "DeleteUserUsecase",
+    "GetUserUsecase",
+    "EditUserUsecase",
+)
--- test/usecase/conftest.py
+++ test/usecase/conftest.py
@@ -1,11 +1,18 @@
 from pytest import fixture
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.usecase.user import RegisterUserUsecase
+from src.usecase.user import RegisterUserUsecase, GetUserUsecase
 
 
 @fixture
 def test_register_usecase(test_engine) -> RegisterUserUsecase:
     session = AsyncSession(test_engine)
     usecase = RegisterUserUsecase(session=session)
+    return usecase
+
+
+@fixture
+def test_get_usecase(test_engine) -> GetUserUsecase:
+    session = AsyncSession(test_engine)
+    usecase = GetUserUsecase(session=session)
     return usecase

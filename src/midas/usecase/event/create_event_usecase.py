da7500122f4551ff5f2434ac6b8359836fb5d437
Artiom Astashonak <artiomastashonak@gmail.com>
2026-01-23 15:46:28 +0100

    feat(usecase): add `CreateEventUsecase` class

---
+++ src/midas/usecase/event/create_event_usecase.py
@@ -0,0 +1,80 @@
+from datetime import date, timedelta
+from decimal import Decimal
+from typing import Optional, override
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from midas.loggers import app_logger
+
+from midas.db.schemas.event import Event
+from midas.query.event import EventRepository
+from midas.query.user import UserRepository
+from midas.usecase.abstract_usecase import AbstractUsecase
+from midas.util.enums import TransactionType
+
+
+class CreateEventUsecase(AbstractUsecase[None]):
+    @override
+    def __init__(self, session: AsyncSession | None = None) -> None:
+        super().__init__(session)
+        self._user_repo = UserRepository(self._session)
+        self._event_repo = EventRepository(self._session)
+
+    @override
+    async def execute(
+        self,
+        user_id: int,
+        transaction_type: TransactionType,
+        title: str,
+        amount: Decimal,
+        update_interval: int,
+        description: Optional[str] = None,
+    ) -> None:
+        """
+        Create a new recurring event.
+
+        :param user_id: user's telegram id
+        :type user_id: int
+        :param transaction_type: transaction type
+        :type transaction_type: TransactionType
+        :param title: transaction title
+        :type title: str
+        :param amount: transaction amount
+        :type amount: Decimal
+        :param update_interval: interval in days of how often
+        the event should be repeated
+        :type update_interval: int
+        :param description: optional transaction description
+        :type description: Optional[str]
+
+        :raise ValueError: if no user with `user_id` exists
+        """
+        app_logger.debug(
+            f"Started `CreateEventUsecase` execution: {user_id} - {transaction_type}"
+        )
+
+        async with self._session:
+            user = await self._user_repo.get_by_id(user_id)
+            if user is None:
+                app_logger.debug(
+                    "Finished `CreateEventUsecase` execution too soon because user does not exist"
+                )
+                raise ValueError(f"No user with {user_id} exists")
+
+            today = date.today()
+            event = Event(
+                user_id=user_id,
+                transaction_type_id=transaction_type,
+                title=title,
+                description=description,
+                amount=amount,
+                last_run_on=today,
+                interval=update_interval,
+                next_run_at=today + timedelta(days=update_interval),
+            )
+            self._event_repo.add(event)
+
+            await self._session.commit()
+
+        app_logger.debug(
+            f"Successfully created an event: {user_id} - {transaction_type}"
+        )

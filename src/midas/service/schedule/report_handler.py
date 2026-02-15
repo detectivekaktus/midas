efb5bf2bb354ee12e5b2b18372711e6d914eef45
Artiom Astashonak <artiomastashonak@gmail.com>
2026-02-15 11:57:19 +0100

    feat(service): finish monthly report handler implementation

--- src/midas/service/schedule/report_handler.py
+++ src/midas/service/schedule/report_handler.py
@@ -2,20 +2,37 @@ from asyncio import sleep
 from calendar import monthrange
 from datetime import date
 from time import perf_counter
-from typing import override
+from typing import Any, override
 
 from midas.loggers import app_logger
 
 from midas.service.abstract_notifier import AbstractNotifier
 from midas.service.schedule.abstract_handler import AbstractHandler
 from midas.usecase.report import GenerateReportUsecase
+from midas.usecase.user import GetAllUsersUsecase
+from midas.util.enums import Currency, TransactionType
 
 
 class ReportHandler(AbstractHandler):
     @override
     def __init__(self, notifier: AbstractNotifier, update_interval: int = 600) -> None:
         super().__init__(notifier, update_interval)
-        self._generate_repo = GenerateReportUsecase()
+        self._generate_report = GenerateReportUsecase()
+        self._get_users = GetAllUsersUsecase()
+
+    def _generate_notifier_message(
+        self, report: dict[str, Any], currency: Currency
+    ) -> str:
+        msg = "MONTHLY REPORT:\n"
+        msg += "\n".join(
+            [
+                f"- {name.title().replace("_", " ")}: {currency.name} {value}"
+                for name, value in report["accounts"].items()
+                if name != TransactionType.INCOME.name.lower()
+            ]
+        )
+        msg += f"\nOverall monthly balance: {currency.name} {report["result"]}"
+        return msg
 
     @override
     async def loop(self) -> None:
@@ -33,7 +50,17 @@ class ReportHandler(AbstractHandler):
                 continue
 
             reports_generated = 0
-            # WIP: missing GetAllUsersUsecase
+            users = await self._get_users.execute()
+            for user in users:
+                report = await self._generate_report.execute(user.id)
+
+                if user.send_notifications:
+                    msg = self._generate_notifier_message(
+                        report, Currency(user.currency_id)
+                    )
+                    await self._notifier.notify(user.id, msg)
+
+                reports_generated += 1
 
             app_logger.info(
                 f"Finished generating {reports_generated} reports in {round(perf_counter() - start, 3)} seconds."
